d5719c7c8ce40032df543bd144d87082f4214eab
unknown <andradhandra@gmail.com>
2022-01-28 16:13:11 +0700

    Add State Management Riverpod

--- lib/persistence/hive_data_store.dart
+++ lib/persistence/hive_data_store.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:habit_tracker_flutter/models/task_model.dart';
+import 'package:habit_tracker_flutter/models/task_state.dart';
 import 'package:hive_flutter/adapters.dart';
 
 class HiveDataStore {
   static const taskBoxName = 'tasks';
+  static const taskStateBoxName = 'tasksState';
+  static String taskStateKey(String key) => 'taskState/$key';
+
   Future<void> init() async {
     await Hive.initFlutter();
     //register adapters
     Hive.registerAdapter<TaskModel>(TaskModelAdapter());
+    Hive.registerAdapter<TaskState>(TaskStateAdapter());
     //open boxes
     await Hive.openBox<TaskModel>(taskBoxName);
   }
@@ -26,4 +32,24 @@ class HiveDataStore {
   ValueListenable<Box<TaskModel>> taskListenable() {
     return Hive.box<TaskModel>(taskBoxName).listenable();
   }
+
+  Future<void> setTaskState({
+    required TaskModel task,
+    required bool isCompleted,
+  }) async {
+    final box = Hive.box<TaskState>(taskStateBoxName);
+    final taskState = TaskState(taskId: task.id, isCompleted: isCompleted);
+    await box.put(taskStateKey(task.id), taskState);
+  }
+
+  ValueListenable<Box<TaskState>> taskStateListenable(
+      {required TaskModel task}) {
+    final box = Hive.box<TaskState>(taskStateBoxName);
+    final key = taskStateKey(task.id);
+    return box.listenable(keys: <String>[key]);
+  }
 }
+
+final dataStoreProvider = Provider<HiveDataStore>((ref) {
+  throw UnimplementedError();
+});
--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:habit_tracker_flutter/models/task_model.dart';
 import 'package:habit_tracker_flutter/persistence/hive_data_store.dart';
 import 'package:habit_tracker_flutter/ui/home/task_grid_page.dart';
 import 'package:hive/hive.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
-    // final box = Hive.box<TaskModel>(HiveDataStore.taskBoxName);
-    final dataStore = HiveDataStore();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final dataStore = ref.watch(dataStoreProvider);
     return ValueListenableBuilder(
       valueListenable: dataStore.taskListenable(),
       builder: (_, Box<TaskModel> box, __) => TaskGridPage(
